5e2ae9b1c82ce3e89ebf4066bfdce3a65ee813b3
Alexandr Sarandi <asarandi@student.42.us.org>
2018-11-27 21:45:07 -0800

    grace, removed write_kid() function

--- Grace.c
+++ Grace.c
@@ -6,20 +6,8 @@
 ** dr_quine [grace], 42 fremont, nov 2018
 */
 
-#define EXEC int main(void) { (void)write_kid(); return 0;};
+#define SELF "#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c%c/*%c** dr_quine [grace], 42 fremont, nov 2018%c*/%c%c#define SELF %c%s%c%c#define DQ 34%c#define EXEC int main(void) { int fd; if ((fd = open(%cGrace_kid.c%c, O_WRONLY | O_CREAT | O_TRUNC, 0644)) > 0) { (void)dprintf(fd,SELF,10,10,10,10,10,10,10,10,DQ,SELF,DQ,10,10,DQ,DQ,10,10,10); (void)close(fd); } return 0;}%c%cEXEC%c"
 #define DQ 34
-#define SELF "#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c%c/*%c** dr_quine [grace], 42 fremont, nov 2018%c*/%c%c#define EXEC int main(void) { (void)write_kid(); return 0;};%c#define DQ 34%c#define SELF %c%s%c%c%cvoid%cwrite_kid(void)%c{%c%cint fd;%c%c%cif ((fd = open(%cGrace_kid.c%c, O_WRONLY | O_CREAT | O_TRUNC, 0644)) > 0)%c%c{%c%c%c(void)dprintf(fd,SELF,10,10,10,10,10,10,10,10,10,10,DQ,SELF,DQ,10,10,9,10,10,9,10,10,9,DQ,DQ,10,9,10,9,9,10,9,9,10,9,10,10,10,10,10);%c%c%c(void)close(fd);%c%c}%c}%c%c%cEXEC%c"
-
-void	write_kid(void)
-{
-	int fd;
-
-	if ((fd = open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC, 0644)) > 0)
-	{
-		(void)dprintf(fd,SELF,10,10,10,10,10,10,10,10,10,10,DQ,SELF,DQ,10,10,9,10,10,9,10,10,9,DQ,DQ,10,9,10,9,9,10,9,9,10,9,10,10,10,10,10);
-		(void)close(fd);
-	}
-}
-
+#define EXEC int main(void) { int fd; if ((fd = open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC, 0644)) > 0) { (void)dprintf(fd,SELF,10,10,10,10,10,10,10,10,DQ,SELF,DQ,10,10,DQ,DQ,10,10,10); (void)close(fd); } return 0;}
 
 EXEC
