d88a86163794c8876fb14121fbc1f4d44b88da7e
Alexandr Sarandi <asarandi@student.42.us.org>
2018-11-27 19:51:53 -0800

    grace cosmetics

--- compile.sh
+++ compile.sh
@@ -3,4 +3,9 @@ gcc -g Colleen.c -o Colleen_c
 nasm -g -f macho64 Colleen.asm -o Colleen_asm.o
 gcc -g Colleen_asm.o -o Colleen_asm
 
-rm -f a.out Grace_kid.c; gcc Grace.c ; ./a.out; ls -ltra Grac* ; md5 Grace.c Grace_kid.c 
+rm -f a.out Grace_kid.c; gcc Grace.c ; ./a.out; ls -ltra Grac* ; md5 Grace.c Grace_kid.c
+
+rm -f Grace_kid* Grace_asm ;
+nasm -f macho64 Grace.s && cc Grace.o
+./a.out
+md5 Grace.s Grace_kid.s
