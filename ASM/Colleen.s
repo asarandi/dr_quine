da193e5543954cfbfcad0d44a26d65186f45f807
Alexandr Sarandi <asarandi@student.42.us.org>
2018-11-30 04:10:06 -0800

    asm colleen with local labels, comments and expanded tabs

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,5 +1,8 @@
+;
+; comment outside program
+;
+
 extern _printf
-extern _exit
 
 global _main
 
@@ -10,60 +13,69 @@ section .text
 
 _self_db:
     push    rbp
-    mov rbp, rsp
+    mov     rbp, rsp
+    push    r13
+    push    r14
 
-    lea r14,[rel self]  ; data
+    lea     r14, [rel self]
 
-_self_loop:
-    mov r13, r14        ; save before increment
-_self_db_scan:
-    mov ax, word [r14]
-    cmp ax, 0x000a
-    jz _self_end_of_data
-    mov al, byte [r14]
-    cmp al, 10
-    jz _self_end_of_line
-    inc r14
-    jmp _self_db_scan
+.loop:
+    mov     r13, r14
+.scan:
+    mov     ax, word [r14]
+    cmp     ax, 0x000a
+    jz      .eof
+    mov     al, byte [r14]
+    cmp     al, 10
+    jz      .eol
+    inc     r14
+    jmp     .scan
 
-_self_end_of_line:
-    mov byte [r14], 0
-    mov rsi, r13
-    lea rdi, [rel fmt1]
-    call _printf
-    inc r14
-    jmp _self_loop
+.eol:
+    mov     byte [r14], 0
+    mov     rsi, r13
+    lea     rdi, [rel fmt1]
+    call    _printf
+    inc     r14
+    jmp     .loop
 
-_self_end_of_data:
-    mov byte [r14], 0
-    mov rsi, r13
-    lea rdi, [rel fmt2]
-    call _printf
+.eof:
+    mov     byte [r14], 0
+    mov     rsi, r13
+    lea     rdi, [rel fmt2]
+    call    _printf
 
-    pop rbp
+    pop     r14
+    pop     r13
+    pop     rbp
     ret
 
 _main:
+
+;
+; comment inside main
+;
     push    rbp
-    mov rbp, rsp
+    mov     rbp, rsp
 
-    lea rdi, [rel self]
-    call _printf
+    lea     rdi, [rel self]
+    call    _printf
 
-    call _self_db
+    call    _self_db
 
-    mov rdi, 0
-    call _exit
-
-    pop rbp
+    mov     rax, 0
+    pop     rbp
     ret
 
 section .data
-fmt1 db 0x64, 0x62, 0x20, 0x22, 0x25, 0x73, 0x22, 0x2c, 0x31, 0x30, 0x0a, 0x00
-fmt2 db 0x64, 0x62, 0x20, 0x22, 0x25, 0x73, 0x22, 0x2c, 0x31, 0x30, 0x2c, 0x30, 0x0a, 0x00
+fmt1        db 100,98,32,34,37,115,34,44,49,48,10,0
+fmt2        db 100,98,32,34,37,115,34,44,49,48,44,48,10,0
 self:
+db ";",10
+db "; comment outside program",10
+db ";",10
+db "",10
 db "extern _printf",10
-db "extern _exit",10
 db "",10
 db "global _main",10
 db "",10
@@ -74,55 +86,61 @@ db "section .text",10
 db "",10
 db "_self_db:",10
 db "    push    rbp",10
-db "    mov rbp, rsp",10
+db "    mov     rbp, rsp",10
+db "    push    r13",10
+db "    push    r14",10
 db "",10
-db "    lea r14,[rel self]  ; data",10
+db "    lea     r14, [rel self]",10
 db "",10
-db "_self_loop:",10
-db "    mov r13, r14        ; save before increment",10
-db "_self_db_scan:",10
-db "    mov ax, word [r14]",10
-db "    cmp ax, 0x000a",10
-db "    jz _self_end_of_data",10
-db "    mov al, byte [r14]",10
-db "    cmp al, 10",10
-db "    jz _self_end_of_line",10
-db "    inc r14",10
-db "    jmp _self_db_scan",10
+db ".loop:",10
+db "    mov     r13, r14",10
+db ".scan:",10
+db "    mov     ax, word [r14]",10
+db "    cmp     ax, 0x000a",10
+db "    jz      .eof",10
+db "    mov     al, byte [r14]",10
+db "    cmp     al, 10",10
+db "    jz      .eol",10
+db "    inc     r14",10
+db "    jmp     .scan",10
 db "",10
-db "_self_end_of_line:",10
-db "    mov byte [r14], 0",10
-db "    mov rsi, r13",10
-db "    lea rdi, [rel fmt1]",10
-db "    call _printf",10
-db "    inc r14",10
-db "    jmp _self_loop",10
+db ".eol:",10
+db "    mov     byte [r14], 0",10
+db "    mov     rsi, r13",10
+db "    lea     rdi, [rel fmt1]",10
+db "    call    _printf",10
+db "    inc     r14",10
+db "    jmp     .loop",10
 db "",10
-db "_self_end_of_data:",10
-db "    mov byte [r14], 0",10
-db "    mov rsi, r13",10
-db "    lea rdi, [rel fmt2]",10
-db "    call _printf",10
+db ".eof:",10
+db "    mov     byte [r14], 0",10
+db "    mov     rsi, r13",10
+db "    lea     rdi, [rel fmt2]",10
+db "    call    _printf",10
 db "",10
-db "    pop rbp",10
+db "    pop     r14",10
+db "    pop     r13",10
+db "    pop     rbp",10
 db "    ret",10
 db "",10
 db "_main:",10
+db "",10
+db ";",10
+db "; comment inside main",10
+db ";",10
 db "    push    rbp",10
-db "    mov rbp, rsp",10
+db "    mov     rbp, rsp",10
 db "",10
-db "    lea rdi, [rel self]",10
-db "    call _printf",10
+db "    lea     rdi, [rel self]",10
+db "    call    _printf",10
 db "",10
-db "    call _self_db",10
+db "    call    _self_db",10
 db "",10
-db "    mov rdi, 0",10
-db "    call _exit",10
-db "",10
-db "    pop rbp",10
+db "    mov     rax, 0",10
+db "    pop     rbp",10
 db "    ret",10
 db "",10
 db "section .data",10
-db "fmt1 db 0x64, 0x62, 0x20, 0x22, 0x25, 0x73, 0x22, 0x2c, 0x31, 0x30, 0x0a, 0x00",10
-db "fmt2 db 0x64, 0x62, 0x20, 0x22, 0x25, 0x73, 0x22, 0x2c, 0x31, 0x30, 0x2c, 0x30, 0x0a, 0x00",10
+db "fmt1        db 100,98,32,34,37,115,34,44,49,48,10,0",10
+db "fmt2        db 100,98,32,34,37,115,34,44,49,48,44,48,10,0",10
 db "self:",10,0
