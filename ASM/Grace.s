391df78a9a065e3ad23d766b4694f8e3d826a330
Alexandr Sarandi <asarandi@student.42.us.org>
2018-12-02 02:43:20 -0800

    even shorter asm grace, thnx stephen

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,186 +1,106 @@
-;
 ; dr_quine [grace], 42 fremont, nov 2018
-;
-
-%define OPEN_MODE   644o
-%define OPEN_FLAGS  0x601
-%define FT          _main
-
-global  FT
-
-extern  _open
-extern  _close
-extern  _dprintf
-
-bits    64
-default rel
-
-section .text
-
-_self_db:
-    push    rbp
-    mov     rbp, rsp
-    push    r13
+%define FMT db 'db ',34,37,'s',34,',10'
+%define GRC _main
+%macro FT 0
+GRC:push    r13
     push    r14
-
-    lea     r14, [rel self]
-
-.loop:
-    mov     r13, r14
-.scan:
-    mov     ax, word [r14]
-    cmp     ax, 0x000a
-    jz      .eof
-    mov     al, byte [r14]
-    cmp     al, 10
-    jz      .eol
-    inc     r14
-    jmp     .scan
-
-.eol:
-    mov     byte [r14], 0
-    mov     rdx, r13
-    lea     rsi, [rel fmt1]
-    mov     rdi, [rel fd]
-    call    _dprintf
-    inc     r14
-    jmp     .loop
-
-.eof:
-    mov     byte [r14], 0
-    mov     rdx, r13
-    lea     rsi, [rel fmt2]
-    mov     rdi, [rel fd]
-    call    _dprintf
-
-    pop     r14
-    pop     r13
-    pop     rbp
-    ret
-
-FT:
-    push    rbp
-    mov     rbp, rsp
-
-    mov     rdx, OPEN_MODE
-    mov     rsi, OPEN_FLAGS
-    lea     rdi, [rel fn]
+    push    r15
+    mov     rdx, 644o
+    mov     rsi, 0x601
+    call    opn
+    db      'Grace_kid.s',0
+opn:pop     rdi
     call    _open
     cmp     eax, 0
-    jle     .end
-    mov     [rel fd], eax
-
+    jle     end
+    mov     r15, rax
     mov     rdi, rax
     lea     rsi, [rel self]
     call    _dprintf
-
-    call    _self_db
-
-    mov     rdi, [rel fd]
+    lea     r14, [rel self]
+lup:mov     r13, r14
+sca:mov     al, byte [r14]
+    inc     r14
+    cmp     al, 10
+    jnz     sca
+    mov     byte [r14 - 1], 0
+    mov     rdx, r13
+    call    fmt
+    FMT,10,0
+fmt:pop     rsi
+    mov     rdi, r15
+    call    _dprintf
+    mov     al, byte [r14]
+    test    al, al
+    jnz     lup
+    mov     rdi, r15
     call    _close
-
-.end:
+end:pop     r15
+    pop     r14
+    pop     r13
     mov     rax, 0
-    pop     rbp
     ret
-
+%endmacro
+bits    64
+default rel
+extern  _open
+extern  _close
+extern  _dprintf
+global  GRC
+section .text
+    FT
 section .data
-    fn      db 'Grace_kid.s',0
-    fd      dd 0
-    fmt1    db 100,98,32,34,37,115,34,44,49,48,10,0
-    fmt2    db 100,98,32,34,37,115,34,44,49,48,44,48,10,0
-
-self:
-db ";",10
+    self:
 db "; dr_quine [grace], 42 fremont, nov 2018",10
-db ";",10
-db "",10
-db "%%define OPEN_MODE   644o",10
-db "%%define OPEN_FLAGS  0x601",10
-db "%%define FT          _main",10
-db "",10
-db "global  FT",10
-db "",10
-db "extern  _open",10
-db "extern  _close",10
-db "extern  _dprintf",10
-db "",10
-db "bits    64",10
-db "default rel",10
-db "",10
-db "section .text",10
-db "",10
-db "_self_db:",10
-db "    push    rbp",10
-db "    mov     rbp, rsp",10
-db "    push    r13",10
+db "%%define FMT db 'db ',34,37,'s',34,',10'",10
+db "%%define GRC _main",10
+db "%%macro FT 0",10
+db "GRC:push    r13",10
 db "    push    r14",10
-db "",10
-db "    lea     r14, [rel self]",10
-db "",10
-db ".loop:",10
-db "    mov     r13, r14",10
-db ".scan:",10
-db "    mov     ax, word [r14]",10
-db "    cmp     ax, 0x000a",10
-db "    jz      .eof",10
-db "    mov     al, byte [r14]",10
-db "    cmp     al, 10",10
-db "    jz      .eol",10
-db "    inc     r14",10
-db "    jmp     .scan",10
-db "",10
-db ".eol:",10
-db "    mov     byte [r14], 0",10
-db "    mov     rdx, r13",10
-db "    lea     rsi, [rel fmt1]",10
-db "    mov     rdi, [rel fd]",10
-db "    call    _dprintf",10
-db "    inc     r14",10
-db "    jmp     .loop",10
-db "",10
-db ".eof:",10
-db "    mov     byte [r14], 0",10
-db "    mov     rdx, r13",10
-db "    lea     rsi, [rel fmt2]",10
-db "    mov     rdi, [rel fd]",10
-db "    call    _dprintf",10
-db "",10
-db "    pop     r14",10
-db "    pop     r13",10
-db "    pop     rbp",10
-db "    ret",10
-db "",10
-db "FT:",10
-db "    push    rbp",10
-db "    mov     rbp, rsp",10
-db "",10
-db "    mov     rdx, OPEN_MODE",10
-db "    mov     rsi, OPEN_FLAGS",10
-db "    lea     rdi, [rel fn]",10
+db "    push    r15",10
+db "    mov     rdx, 644o",10
+db "    mov     rsi, 0x601",10
+db "    call    opn",10
+db "    db      'Grace_kid.s',0",10
+db "opn:pop     rdi",10
 db "    call    _open",10
 db "    cmp     eax, 0",10
-db "    jle     .end",10
-db "    mov     [rel fd], eax",10
-db "",10
+db "    jle     end",10
+db "    mov     r15, rax",10
 db "    mov     rdi, rax",10
 db "    lea     rsi, [rel self]",10
 db "    call    _dprintf",10
-db "",10
-db "    call    _self_db",10
-db "",10
-db "    mov     rdi, [rel fd]",10
+db "    lea     r14, [rel self]",10
+db "lup:mov     r13, r14",10
+db "sca:mov     al, byte [r14]",10
+db "    inc     r14",10
+db "    cmp     al, 10",10
+db "    jnz     sca",10
+db "    mov     byte [r14 - 1], 0",10
+db "    mov     rdx, r13",10
+db "    call    fmt",10
+db "    FMT,10,0",10
+db "fmt:pop     rsi",10
+db "    mov     rdi, r15",10
+db "    call    _dprintf",10
+db "    mov     al, byte [r14]",10
+db "    test    al, al",10
+db "    jnz     lup",10
+db "    mov     rdi, r15",10
 db "    call    _close",10
-db "",10
-db ".end:",10
+db "end:pop     r15",10
+db "    pop     r14",10
+db "    pop     r13",10
 db "    mov     rax, 0",10
-db "    pop     rbp",10
 db "    ret",10
-db "",10
+db "%%endmacro",10
+db "bits    64",10
+db "default rel",10
+db "extern  _open",10
+db "extern  _close",10
+db "extern  _dprintf",10
+db "global  GRC",10
+db "section .text",10
+db "    FT",10
 db "section .data",10
-db "    fn      db 'Grace_kid.s',0",10
-db "    fd      dd 0",10
-db "    fmt1    db 100,98,32,34,37,115,34,44,49,48,10,0",10
-db "    fmt2    db 100,98,32,34,37,115,34,44,49,48,44,48,10,0",10
-db "",10
-db "self:",10,0
+db "    self:",10
